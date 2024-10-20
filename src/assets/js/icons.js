858a8124bf29f986b691a4af0559202a802a8631
brian deng <69863366+br-iscool@users.noreply.github.com>
2024-10-19 21:51:57 -0600

    Add files via upload

---
+++ src/assets/js/icons.js
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenNib } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+const icons = () => {
+  return (
+    <div>icons</div>
+  )
+}
+
+export default icons
---
+++ src/main.tsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import Navbar from './components/Navbar.tsx'
+import Home from './components/Home.tsx'
+import About from './components/About.tsx'
+
+import './css/index.css'
+import './css/scroll-animate.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+
+    <>
+      {/* TODO
+        Do about me section
+        Make the header look better (centered island?)
+        animate on scroll? (testing with keyframes)
+        delay on shader (3s)?
+        compatible on mobile (center the divs and make it overlap)
+        make it so that box-shader doesnt break when resizing window
+        turn off / on shader button
+        Go through css and remove redundant (from .nav-logo)
+        style emoji?
+
+        */}
+
+      <Navbar/>
+      <Home/>
+      <About/>
+    </>
+
+  </StrictMode>,
+)

940f273e1d23bbcdbf3342adf706015692fa2438
Brian Deng <brian.d3.2008@gmail.com>
2024-11-14 23:09:39 -0700

    Update docs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,26 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 
 export default function Home() {
-  return (
-    <>
-      {/* TODO
-        Readd shader support
-        Do about me section
-        Remove redundant css and convert .css to tailwind and remove css folder
-        Make the header look better (centered island?)
-        animate on scroll? (testing with keyframes)
-        <noscript> tag
-        compatible on mobile (center the divs and make it overlap)
-        make it so that box-shader doesnt break when resizing window
-        turn off / on shader button
-        Make footer 
+  	return (
+    	<>
+      		{/* TODO
+			Readd shader support
+			Figure out props / and better project layout and structure
+			Do about me section
+			Remove redundant css and convert .css to tailwind and remove css folder
+			Make the header look better (centered island?)
+			animate on scroll? (testing with keyframes)
+			<noscript> tag
+			compatible on mobile (center the divs and make it overlap)
+			make it so that box-shader doesnt break when resizing window
+			turn off / on shader button
+			Make footer 
 
-        */}
+			*/}
 
-      <Navbar/>
-      <Hero/>
-      <About/>
-    </>
-  );
+			<Navbar />
+			<Hero />
+			<About />
+    	</>
+  	);
 }

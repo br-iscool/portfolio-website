88d809411847876a8654ca25869d797023300bee
Brian Deng <69863366+br-iscool@users.noreply.github.com>
2025-03-01 21:05:04 -0700

    feat: recoded hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,31 +1,29 @@
 import Image from "next/image";
+import HeroButton from "@/components/ui/HeroButton";
 import { inter } from "@/utils/fonts";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Hero() {
-    return (
-        <section className={`home box-shadow flex min-h-screen items-center justify-center gap-20 p-12 ${inter.className}`}>
-            <div className="w-full relative flex flex-row gap-15 justify-center items-center px-5">
-                <div className="w-[50%] h-full flex flex-col justify-center gap-4 text-white">
-                    <p className="text-lg font-light">
-                        Hi, I'm
-                    </p>
-                    <h1 className="text-6xl font-extrabold">
-                        Brian Deng
-                    </h1>
-                    <p className="text-2xl font-thin">
-                        Full Stack Developer
-                    </p>
-                    <p className="text-md font-light mt-4">
-                        Lorem Ipsum Description
-                    </p>
-                    <button className="mt-6 px-6 py-3 bg-white text-black font-semibold rounded-lg shadow-md hover:bg-gray-100 transition duration-300 z-50">
-                        Contact Me
-                    </button>
-                </div>
-                <div className="">
-                    <Image src="/favicon.ico" alt="Headshot" width={250} height={250} className="rounded-full border-2 border-white shadow-lg" loading="eager" />
-                </div>
-            </div>
-        </section>
-    );  
+	return (
+		<section className={`home box-shadow flex min-h-screen items-center justify-center gap-20 p-12 ${inter.className}`}>
+			<div className="w-full relative flex flex-row gap-40 justify-center items-center">
+				<div className="w-2xl h-full flex flex-col justify-center gap-4 text-white">
+					<p className="text-lg font-light">Hi, I'm</p>
+					<h1 className="text-8xl font-extrabold">Brian Deng</h1>
+					<p className="text-xl font-extralight">Full stack developer based in Canada.</p>
+
+                    <div className="flex flex-row gap-4 pt-4">
+                        <HeroButton icon={faLinkedin} href="https://www.linkedin.com" />
+                        <HeroButton icon={faGithub} href="https://github.com/br-iscool" />
+                        <HeroButton icon={faEnvelope} href="mailto:" />
+                    </div>
+				</div>
+
+				<div className="">
+					<Image src="/favicon.ico" alt="Headshot" width={250} height={250} className="rounded-full border-2 border-white shadow-lg" loading="eager" />
+				</div>
+			</div>
+		</section>
+	);
 }
---
+++ src/components/ui/HeroButton.tsx
@@ -0,0 +1,23 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as motion from "motion/react-client";
+
+interface HeroButtonProps {
+    icon: IconDefinition;
+    className?: string;
+    href?: string;
+}
+
+export default function HeroButton({ icon, className, href }: HeroButtonProps) {
+    return (
+        <a href={href} target="_blank">
+            <motion.button
+                className={`bg-black w-12 h-12 cursor-pointer border-[#7982ef] border-2 rounded-[50%] ${className}`}
+                whileHover={{ scale: 1.05, rotate: -15 }}
+                transition={{ duration: 0.3 }}
+            >
+                <FontAwesomeIcon icon={icon} className="" />
+            </motion.button>
+        </a>
+    );
+}

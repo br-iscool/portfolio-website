16a42eaab68dff77ea693273341b0f8328f60807
Brian Deng <69863366+br-iscool@users.noreply.github.com>
2025-03-02 17:24:40 -0700

    feat: shader toggle switch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@
 import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 import { useState } from "react";
 import Link from "next/link";
-import Button from "../components/ui/Button";
+import NavButton from "./ui/NavButton";
 
 export default function Navbar() {
     const [hidden, setHidden] = useState(false);
@@ -21,9 +21,13 @@ export default function Navbar() {
             animate={{ y: hidden ? -100 : 0 }}
             transition={{ duration: 0.3, ease: "easeInOut" }}
         >
-            <div className="flex items-center justify-center pl-5">
-                <Link href={"/"} className="py-4 text-xs font-bold duration-75 hover:scale-110">{"Home"}</Link>
-            </div>
+            <motion.div 
+                className="flex items-center justify-center pl-5"
+                whileHover={{ scale: 1.1 }}
+                transition={{ duration: 0.3}}
+            >
+                <Link href={"/"} className="py-4 text-xs font-bold duration-75">{"Home"}</Link>
+            </motion.div>
             <ul className="w-full flex-col justify-center gap-10 max-sm:gap-5 max-sm:pt-10 sm:flex sm:flex-row">
                 <li>
                     <Link href={"#about"} className="py-4 text-[12.5px]">{"About"}</Link>
@@ -37,7 +41,7 @@ export default function Navbar() {
             </ul>
 
             <div className="flex items-center justify-center">
-                <Button />
+                <NavButton />
             </div>
         </motion.header>
     );
---
+++ src/components/ui/NavButton.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+
+export default function NavButton() {
+    const [isShaderEnabled, setIsShaderEnabled] = useState(true);
+    const [isBoxShadowEnabled, setIsBoxShadowEnabled] = useState(true);
+    const [isChecked, setIsChecked] = useState(false);
+
+    const toggleShader = () => {
+        if (isShaderEnabled) {
+            document.querySelector("canvas")?.classList.add("hidden");
+        } else {
+            document.querySelector("canvas")?.classList.remove("hidden");
+        }
+        setIsShaderEnabled(!isShaderEnabled);
+    };
+
+    const toggleBoxShadow = () => {
+        const heroSection = document.querySelector(".home");
+        if (isBoxShadowEnabled) {
+            heroSection?.classList.remove("box-shadow");
+        } else {
+            heroSection?.classList.add("box-shadow");
+        }
+        setIsBoxShadowEnabled(!isBoxShadowEnabled);
+    };
+
+    const handleClick = () => {
+        toggleShader();
+        toggleBoxShadow();
+        setIsChecked(!isChecked);
+    };
+
+    return (
+        <label className="flex cursor-pointer select-none items-center">
+        <div className="relative">
+            <input
+                type="checkbox"
+                checked={isChecked}
+                onChange={handleClick}
+                className="sr-only"
+            />
+            <div className="block h-7 w-13 rounded-full bg-gray-300"></div>
+            <div
+                className={`dot absolute left-1 top-1 flex h-5 w-5 items-center justify-center rounded-full bg-white transition-transform ${
+                    isChecked ? "" : "translate-x-6"
+                }`}
+            >
+                {isChecked ? (
+                    <svg
+                        className="h-3 w-3 text-gray-800 stroke-current"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M6 18L18 6M6 6l12 12"
+                        ></path>
+                    </svg>  
+                ) : (
+                    <svg
+                        className="h-3 w-3 text-gray-800 stroke-current"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M5 13l4 4L19 7"
+                        ></path>
+                    </svg>
+                )}
+            </div>
+        </div>
+        </label>
+    );
+}

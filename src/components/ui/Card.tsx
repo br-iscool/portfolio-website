9c66c56eae188782989a6e183e4ef26cabd85988
Brian Deng <69863366+br-iscool@users.noreply.github.com>
2025-03-08 21:31:26 -0700

    chore: resize components

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 // Separate each description with a fragment (<>, </>) when using this component
 export default function Card({ title, description, id }: CardProps) {
     return (
-        <section className="bg-black h-full py-36 text-white" id={id}>
+        <section className="bg-black h-full pt-36 text-white" id={id}>
             <div className="max-w-5xl mx-auto">
                 <div className="flex flex-col items-center justify-center p-10 bg-[#262626] rounded-xl">
                     <div className="w-full text-left">

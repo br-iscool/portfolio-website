abc17a810712108c0fea12f5b4908b6e3c67cbf9
Brian Deng <69863366+br-iscool@users.noreply.github.com>
2026-02-13 23:59:25 -0700

    feat: add noscript to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { inter } from "@/utils/fonts";
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import Link from "next/link";
 config.autoAddCss = false;
 
 export const metadata: Metadata = {
@@ -24,7 +25,20 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${inter.className} antialiased`} suppressHydrationWarning>{children}</body>
+            <body className={`${inter.className} antialiased`} suppressHydrationWarning>
+                <noscript>
+                    <div className="flex items-center text-center justify-center p-8">
+                        <Link
+                            className="text-lg underline underline-offset-4 opacity-90"
+                            href="https://www.enable-javascript.com/"
+                            target="_blank"
+                        >
+                            This page cannot be viewed, please enable JavaScript and try again.
+                        </Link>
+                    </div>
+                </noscript>
+                {children}
+            </body>
         </html>
     );
 }
